ef69c8d581f41b8844a2ae9e4465376620400f21
Simon Lervad <lervad1996@gmail.com>
2019-09-20 12:37:16 +0200

    rettelse

---
+++ head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Spil portalen</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<h1>Spilportalen</h1>
+	</header>
--- number.php
+++ number.php
@@ -1,5 +1,14 @@
+<?php include 'head.php' ?>
 <div id="number">
 	<div class="header">
 		<h2>You are playing Numbers</h2>
+		<form>
+			<input id="gues" type="text" name="gues">
+			<input type="submit" name="submit" value="Gæt">
+		</form>
 	</div>
+	<script>
+		var x = 120;
+		var y = document.getElementById("gues").value;
+	</script>
 </div>
