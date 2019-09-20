be646b37151746527c4d5f7b49c31d33556e8a9e
Simon Lervad <lervad1996@gmail.com>
2019-09-20 15:48:09 +0200

    tæller tilføjet

--- number.php
+++ number.php
@@ -2,15 +2,39 @@
 <div id="number">
 	<div class="header">
 		<h2>Number games</h2>
-		<form>
 			<input id="gues" type="number" name="number">
-			<input type="submit" name="submit" value="gæt">
-		</form>
+			<button id="knap">Gæt</button>
+			<p id="demo"></p>
+			<a id="dead" href=""></a>
 	</div>
 </div>
 <script>
-	var x = 120;
-	var y = document.getElementById("gues").value;
+	var i = 1;
+	const gues = function() {
+		if (i === 10) {
+			document.getElementById("dead").innerHTML = "Du har tabt <br> spil igen";
+		} else if (i < 10) {
+			i++;
+		}
+	}
+	const x = Math.floor(Math.random() * 1000);
+	const $ = function(foo) {
+    	return document.getElementById(foo);    
+	}
+	const validate = function() {    	
+    	if ($("gues").value < x) {
+    		document.getElementById("demo").innerHTML = $("gues").value + " er for lavt";
+    	} else if ($("gues").value > x) {
+    		document.getElementById("demo").innerHTML = $("gues").value + " er for højt";
+    	} else {
+    		window.alert("Korrekt" + "<a href=''>play again</a>")
+    	}
+	}
+	const init = function() {
+		$("knap").addEventListener('click', validate);	
+		$("knap").addEventListener('click', gues);	
+}
+	window.addEventListener('load', init);
 </script>
 </body>
 </html>
