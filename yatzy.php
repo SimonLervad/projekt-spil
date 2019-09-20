2e2bf759c282a065163859c57c089cb06ff1f8ef
tinnamrichter <tinn0229@easv365.dk>
2019-09-20 12:43:53 +0200

    yatzy ændringer

--- yatzy.php
+++ yatzy.php
@@ -1,5 +1,26 @@
+<?php include 'head.php'?>
 <div id="yatzy">
 	<div class="header">
 		<h2>You are playing yatzy</h2>
 	</div>
 </div>
+
+<script>
+'use strict';
+// terninger
+let t1 = 0;
+let t2 = 0;
+let t3 = 0;
+let t4 = 0;
+let t5 = 0;
+
+// slå med terninger
+let flip = Math.floor((Math.random() * 5 + 1);
+
+
+
+
+
+
+
+</script>
